bd031955fc030ef0285b4c9399fae97434f24e19
JT Wright <wright.jacob25@svvsd.org>
2016-09-10 22:16:38 -0600

    🌇👨 0.0.0a

--- public/client.js
+++ public/client.js
@@ -1,35 +1,89 @@
-//Function that runs at start of program, shows google map
-//Zooms map to specific distance wanted
-//Shows where the user is, given the user shares geocoords
 function initMap() {
-  var mapDiv = document.getElementById('map');
-  var map = new google.maps.Map(mapDiv, {
-  center: {lat: 44.540, lng: -78.546},
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+  startPos = "";
+  endPos = "";
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {
+      lat: -34.397,
+      lng: 150.644
+    },
     zoom: 16
   });
-  
-  var userPosMark = new google.maps.Marker({map: map});
+  directionsDisplay.setMap(map);
+  var userMarker = new google.maps.Marker({
+    map: map
+  });
+
+  // Try HTML5 geolocation.
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-    var pos = {
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
-    };
+      pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
 
-    userPosMark.setPosition(pos);
-    map.setCenter(pos);
-    }, function() {
-      handleLocationError(true, userPosMark, map.getCenter());
+      userMarker.setPosition(pos);
+      map.setCenter(pos);
     });
-  } else {
-    // Browser doesn't support Geolocation
-     handleLocationError(false, userPosMark, map.getCenter());
   }
+  var onChangeHandler = function() {
+    calculateAndDisplayRoute(directionsService, directionsDisplay, pos);
+  };
+  google.maps.event.addListener(map, 'click', function(event) {
+    endPosMarker = new google.maps.Marker({position: event.latLng, map: map});
+    endPos = event.latLng;
+    calculateAndDisplayRoute (directionsService, directionsDisplay);
+    endPosMarker.setMap(null);
+});
+  google.maps.event.addListener(map, 'rightclick', function(event) {
+    startPosMarker = new google.maps.Marker({position: event.latLng, map : map});
+    startPos = event.latLng;
+    calculateAndDisplayRoute (directionsService, directionsDisplay);
+    startPosMarker.setMap(null);
+  });
 }
 
-function handleLocationError(browserHasGeolocation, userPosMark, pos) {
-  userPosMark.setPosition(pos);
-  userPosMark.setContent(browserHasGeolocation ?
-    'Error: The Geolocation service failed.' :
-    'Error: Your browser doesn\'t support geolocation.');
+function calculateAndDisplayRoute (directionsService, directionsDisplay) {
+  if(startPos == ""){
+    directionsService.route({
+    origin: pos,
+    destination: endPos,
+    travelMode: 'WALKING'
+  }, function(response,status) {
+    if(status === "OK") {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions failed due to ' + status);
+    }
+  });
+  }
+  else if (endPos === "") {
+    directionsService.route({
+    origin: startPos,
+    destination: pos,
+    travelMode: 'WALKING'
+  }, function(response,status) {
+    if(status === "OK") {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions failed due to ' + status);
+    }
+  });
+  } else {
+    directionsService.route({
+    origin: startPos,
+    destination: endPos,
+    travelMode: 'WALKING'
+  }, function(response,status) {
+    if(status === "OK") {
+      directionsDisplay.setDirections(response);
+    } else {
+      window.alert('Directions failed due to ' + status);
+    }
+  });
+  }
+  
 }
+
+
